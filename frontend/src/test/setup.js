34b42838cb5ebfd6ffbc6f6fd3f06cc03527fe0b
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-17 02:01:36 +0530

    fix: add IntersectionObserver mock for Framer Motion tests

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom/vitest';
+
+class IntersectionObserverMock {
+  constructor(callback) { this.callback = callback; }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.IntersectionObserver = IntersectionObserverMock;
