5602e49a450e99f3d93647143e965009d7d2123e
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-17 00:34:37 +0530

    feat: add projects REST endpoint

---
+++ backend/src/main/java/com/prashant/portfolio/controller/ProjectController.java
@@ -0,0 +1,26 @@
+package com.prashant.portfolio.controller;
+
+import com.prashant.portfolio.entity.Project;
+import com.prashant.portfolio.service.ProjectService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+@CrossOrigin(origins = "*")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+}
