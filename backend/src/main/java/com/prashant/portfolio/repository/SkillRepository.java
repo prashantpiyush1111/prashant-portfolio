5d15b5feb677e3d7aa984288734c010da23e252b
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-17 02:08:10 +0530

    fix: make skill seeding idempotent by name

--- backend/src/main/java/com/prashant/portfolio/repository/SkillRepository.java
+++ backend/src/main/java/com/prashant/portfolio/repository/SkillRepository.java
@@ -1,4 +1,8 @@
 package com.prashant.portfolio.repository;
+
 import com.prashant.portfolio.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface SkillRepository extends JpaRepository<Skill, Long> { }
+
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+    boolean existsByNameIgnoreCase(String name);
+}
