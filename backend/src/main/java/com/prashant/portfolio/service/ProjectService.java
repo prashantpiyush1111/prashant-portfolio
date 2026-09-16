f22774f2880e2aec7223102afc40c99ce3c6161b
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-17 00:34:32 +0530

    feat: add project service

---
+++ backend/src/main/java/com/prashant/portfolio/service/ProjectService.java
@@ -0,0 +1,20 @@
+package com.prashant.portfolio.service;
+
+import com.prashant.portfolio.entity.Project;
+import com.prashant.portfolio.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+}
