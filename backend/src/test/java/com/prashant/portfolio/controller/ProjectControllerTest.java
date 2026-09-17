874253df7df778a1d66ec740f16f961900350088
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-17 12:58:45 +0530

    fix: import Spring component filter type
    Signed-off-by: Prashant Maurya <prashantpiyush1111@gmail.com>

--- backend/src/test/java/com/prashant/portfolio/controller/ProjectControllerTest.java
+++ backend/src/test/java/com/prashant/portfolio/controller/ProjectControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,7 +23,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(value = ProjectController.class, excludeFilters = @ComponentScan.Filter(type = ComponentScan.FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class))
+@WebMvcTest(value = ProjectController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class))
 class ProjectControllerTest {
     @Autowired MockMvc mockMvc;
     @MockBean ProjectService service;
