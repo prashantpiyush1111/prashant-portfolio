b79e507ed078286b920b5f7ee743bf6735e5cbb6
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-19 00:14:04 +0530

    chore: harden portfolio deployment

--- backend/src/main/java/com/prashant/portfolio/config/BrevoMailConfig.java
+++ backend/src/main/java/com/prashant/portfolio/config/BrevoMailConfig.java
@@ -1,7 +1,6 @@
 package com.prashant.portfolio.config;
 
 import java.util.Properties;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,25 +9,20 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class BrevoMailConfig {
-
     @Bean("brevoMailSender")
     public JavaMailSender brevoMailSender(
-            @Value("${BREVO_SMTP_HOST}") String host,
-            @Value("${BREVO_SMTP_PORT}") int port,
-            @Value("${BREVO_SMTP_USERNAME}") String username,
-            @Value("${BREVO_SMTP_PASSWORD}") String password) {
-
+            @Value("${BREVO_SMTP_HOST:}") String host,
+            @Value("${BREVO_SMTP_PORT:587}") int port,
+            @Value("${BREVO_SMTP_USERNAME:}") String username,
+            @Value("${BREVO_SMTP_PASSWORD:}") String password) {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
         mailSender.setHost(host);
         mailSender.setPort(port);
         mailSender.setUsername(username);
         mailSender.setPassword(password);
-
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-
         return mailSender;
     }
 }
