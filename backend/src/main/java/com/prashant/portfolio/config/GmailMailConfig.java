5296eeb2be39ec2de193fdce83f213bf2efcb56b
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-19 00:39:35 +0530

    fix: add SMTP timeouts to prevent contact form hang

--- backend/src/main/java/com/prashant/portfolio/config/GmailMailConfig.java
+++ backend/src/main/java/com/prashant/portfolio/config/GmailMailConfig.java
@@ -30,6 +30,9 @@ public class GmailMailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.connectiontimeout", "5000");
+        props.put("mail.smtp.timeout", "5000");
+        props.put("mail.smtp.writetimeout", "5000");
 
         return mailSender;
     }
