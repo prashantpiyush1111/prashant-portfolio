7a2811ff686fc2ccac78b9facf8b4a56a40a98d0
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-20 14:43:27 +0530

    ci: document frontend lint configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig } from 'eslint/config';
 
+// React/Vite flat ESLint configuration.
 export default defineConfig([
   {
     ignores: ['dist', 'node_modules'],
