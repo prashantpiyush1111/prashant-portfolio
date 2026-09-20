dee5e83cd6e4904d251402619ac51cc3f7aa7050
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-20 14:42:58 +0530

    ci: configure eslint for React JSX

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig } from 'eslint/config';
@@ -10,6 +11,17 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     extends: [js.configs.recommended, reactHooks.configs.flat.recommended],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     plugins: {
       'react-refresh': reactRefresh,
     },
